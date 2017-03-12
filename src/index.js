81411c1878797fdd8a052e2941579921abec6bf6
aaronCKeel <aaronckeel@gmail.com>
2017-03-12 13:14:31 -0700

    add setup for react and jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import Hello from './components/Hello.js';
 
 const greeter = new Hello();
 
 console.log(`index.js says ${greeter.greet()}`);
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('scroll')
+);
