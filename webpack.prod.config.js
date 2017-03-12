533a7b0157f5cd8a1c4031aa0e5bc7a0b9befbdd
aaronCKeel <aaronckeel@gmail.com>
2017-03-11 23:15:54 -0800

    setup production

---
+++ webpack.prod.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+};
