ef0922318c680af97053cb398cb6bf40a940ae03
aaronCKeel <aaronckeel@gmail.com>
2017-03-18 19:52:07 -0700

    add Scroll component to index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Hello from './components/Hello.js';
-import Scroll2 from './components/Scroll2.jsx';
+import Scroll from './components/Scroll.jsx';
 
 const greeter = new Hello();
 
 console.log(`index.js says ${greeter.greet()}`);
 
 ReactDOM.render(
-  <Scroll2 />,
+  <Scroll />,
   document.getElementById('scroll')
 );
