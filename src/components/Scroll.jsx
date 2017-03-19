ca8bb705182491f927318088f37b01581f890559
aaronCKeel <aaronckeel@gmail.com>
2017-03-18 19:53:39 -0700

    remove extra file, create functioning scroll component

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,76 +1,153 @@
 import React from 'react';
-import { List } from 'react-virtualized';
+import ReactDOM from 'react-dom';
 import { SingleSelect } from 'ihme-ui';
 
-function getList(n, strId = 'A') {
-  const listOfItems = [];
+function getOptions(group, n) {
+  const listOfOptions = [];
   for (let i = 0; i < n; i++) {
-    listOfItems.push({ item: `${strId} Item - ${i + 1}` });
+    listOfOptions.push({
+      label: `${group} -- ${i + 1} -- ${group}`,
+      key: i + 1,
+      group,
+    });
   }
 
-  return listOfItems;
+  return listOfOptions;
 }
+
+const containerStyle = {
+  height: '400px',
+  width: '200px',
+  overflowY: 'auto',
+  overflowX: 'hidden',
+};
+
+const selectContainerStyle = {
+  height: '70px',
+};
+
+const selectGroups = [
+  'lamp eye missing',
+  'chair horn head',
+  'coaster fish teeth',
+  'plug pine paint',
+  'composting ring metal',
+  'tower board pillow',
+  'wine shade fyler'
+];
 
 export default class Scroll extends React.Component {
   constructor() {
     super();
-    this.list = getList(100);
-    this.subList = getList(20, 'SuperlongSuperlong');
-    this.rowRenderer = this.rowRenderer.bind(this);
-    this.onChangeSelect = this.onChangeSelect.bind(this);
 
     this.state = {
-      selectValue: null
+      settings: {},
+      menuContainerStyle: {
+        position: 'fixed',
+        zIndex: 10,
+      },
+    };
+
+    this.selectRefs = {};
+
+    this.storeRef = this.storeRef.bind(this);
+    this.onScroll = this.onScroll.bind(this);
+  }
+
+  storeRef(select) {
+    this.selectRefs[select.props.name] = select;
+  }
+
+  onScroll() {
+    if (!this.openedSelect) return;
+
+    const position = ReactDOM.findDOMNode(this.openedSelect).getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    const top = position.top + position.height + 248 > windowHeight
+      ? position.top - 248
+      : position.top + position.height;
+    this.setState(Object.assign(
+      {},
+      this.state,
+      {
+        menuContainerStyle: Object.assign(
+          {},
+          {
+            position: 'fixed',
+            zIndex: 10,
+            top,
+            left: position.left,
+          }
+        )
+      }
+    ));
+  }
+
+  setMenuContainerStyle() {
+    const that = this;
+
+    return function computeMenuContainerStyle() {
+      // this = props of select that is focused.
+      // that = this of Scroll class instance.
+
+      that.openedSelect = that.selectRefs[this.name];
+
+      const position = ReactDOM.findDOMNode(that.selectRefs[this.name]).getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+      const top = position.top + position.height + 248 > windowHeight
+        ? position.top - 248
+        : position.top + position.height;
+      that.setState(Object.assign(
+        {},
+        that.state,
+        { menuContainerStyle: Object.assign(
+          {},
+          {
+            position: 'fixed',
+            zIndex: 10,
+            top,
+            left: position.left,
+          }
+        ) }
+      ));
     };
   }
 
-  onChangeSelect(selection) {
-    this.setState({
-      selectValue: selection
-    });
-  }
+  selectRenderer() {
+    return selectGroups.map((selectGroup, index) => {
+      const options = getOptions(selectGroup, 12);
 
-  rowRenderer({
-    key,
-    index,
-    isScrolling,
-    isVisible,
-    style,
-  }) {
-    Object.assign(style, { direction: 'ltr', paddingLeft: '20px', border: '1px solid #cdc' });
-    return (
-      <div
-        key={key}
-        style={style}
-      >
+      return (
+        <div
+          key={index}
+          style={selectContainerStyle}
+        >
+        <div>{selectGroup}</div>
         <div>
-          {this.list[index].item}
-          {isVisible ? ' V' : null}
-          {isScrolling ? ' S' : null}
+          <SingleSelect
+            ref={this.storeRef}
+            name={selectGroup}
+            labelKey="label"
+            valueKey="key"
+            options={options}
+            width={150}
+            onFocus={this.setMenuContainerStyle()}
+            menuContainerStyle={this.state.menuContainerStyle}
+          />
         </div>
-        <SingleSelect
-          labelKey="item"
-          valueKey="item"
-          options={this.subList}
-          onChange={this.onChangeSelect}
-          value={this.state.selectValue}
-          width={150}
-        />
-      </div>
-    );
+        </div>
+      );
+    });
   }
 
   render() {
     return (
-      <List
-        width={300}
-        height={300}
-        rowCount={this.list.length}
-        rowHeight={60}
-        rowRenderer={this.rowRenderer}
-        selectValue={this.state.selectValue}
-        style={{ direction: 'rtl', textAlign: 'left', border: '1px solid #dcd' }}
-      />
+      <div
+        style={containerStyle}
+        onScroll={this.onScroll}
+      >
+        {this.selectRenderer()}
+      </div>
     );
   }
 }
