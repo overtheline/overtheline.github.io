669dc6c4c528ff365e9aa58d3606c5ae4adf7f05
aaronCKeel <aaronckeel@gmail.com>
2017-06-30 22:42:11 -0700

    reset

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const svgContainer = d3.select('.svg-container');
+
+svgContainer.append('svg')
+    .attr('id', 'svg-one')
+    .attr('height', 500)
+    .attr('width', 800)
+
+const svgOne = d3.select('#svg_one');
+const numberGenerator = d3.randomNormal(1, 0.3);
+
+const data = Array(20).fill(1).map(() => numberGenerator());
+console.log(data);
