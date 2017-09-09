07754bc722e7ccc50a7e4f1e1025f5649f72d35d
overtheline <aaronckeel@gmail.com>
2017-09-09 15:43:52 -0700

    rest of it

--- src/game/utils/checker-fill.ts
+++ src/game/utils/checker-fill.ts
@@ -1,5 +1,3 @@
-import { red, black } from '../constants/colors';
-
-export default function checkerFill(i: number, j: number): string {
-  return (i + j) % 2 === 0 ? black : red;
+export default function checkerFill(i: number, j: number, fillA: string, fillB: string): string {
+  return (i + j) % 2 === 0 ? fillA : fillB;
 }
