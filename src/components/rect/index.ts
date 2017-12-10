eff853b8602d367f8ce65fb46896b2efed97623c
overtheline <aaronckeel@gmail.com>
2017-12-09 17:57:03 -0800

    use update

--- src/components/rect/index.ts
+++ src/components/rect/index.ts
@@ -1,12 +1,22 @@
 import { D3ScaleLinear, D3Selection } from '../../types';
 import BaseComponent from '../base-component';
 
+interface IProps {
+	width: number;
+	height: number;
+	x: number;
+	y: number;
+	fill: string;
+	class: string;
+}
+
 export default class Rect extends BaseComponent {
 	width: number;
 	height: number;
 	x: number;
 	y: number;
 	scale: D3ScaleLinear;
+	element: D3Selection;
 
 	static createRectFactory(target: D3Selection, scale: D3ScaleLinear) {
 		return (width: number, height: number, x: number, y: number) => new Rect(target, width, height, x, y, scale);
@@ -21,13 +31,20 @@ export default class Rect extends BaseComponent {
 		this.scale = scale;
 	}
 
+	update(props: Partial<IProps>) {
+		for (let key in props) {
+			const values: {[key: string]: any} = props;
+			this.element.attr(key, values[key]);
+		}
+	}
+
 	render() {
-		this.target.append('rect')
+		this.element = this.target.append('rect')
 				.attr('width', this.scale(this.width))
 				.attr('height', this.scale(this.height))
 				.attr('x', this.scale(this.x))
 				.attr('y', this.scale(this.y))
-				.attr('fill', 'rgba(255, 0, 0, 0.1')
+				.attr('fill', 'rgba(255, 0, 0, 0')
 				.classed('rect', true);
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
+import { timer } from 'd3-timer';
 import Svg from './components/svg';
 import Rect from './components/rect';
 import { lfsr } from './utils/lfsr';
 
-const width = 265;
-const height = width;
-const domain = [0, 16];
+const tileX = 65;
+const tileY = 63;
+const tileW = 5;
+const width = tileX * tileW;
+const height = tileY * tileW;
+const domain = [0, tileX];
 const range = [0, width];
 const scale = scaleLinear().domain(domain).range(range);
+
+const colors = ['Red', 'Indigo', 'Steelblue', 'Tomato', 'RosyBrown', 'Gold'];
+
+function getFill(count: number) {
+	return colors[count];
+}
 
 const d3Content = select('#content');
 
@@ -18,26 +28,43 @@ const rectFactory = Rect.createRectFactory(lfsrSvg.getSelection(), scale);
 
 const rects: Rect[] = [];
 
-for (let i = 0; i < domain[1]; i++) {
-	for (let j = 0; j < domain[1]; j++) {
+for (let i = 0; i < tileX; i++) {
+	for (let j = 0; j < tileY; j++) {
 		rects.push(rectFactory(1, 1, i, j));
 	}
 }
 
-const tap = parseInt('B2', 16);
+rects.forEach(rect => rect.render());
+
+const taps = [
+'8EF',
+'906',
+'91B',
+'91E',
+'933',
+'939',
+'93F',
+];
+
 let count = 0;
-const first = lfsr(1, tap);
+let tap = parseInt(taps[count], 16);
+const first = lfsr((tileX * tileY + 1) / 2, tap);
 let last = first;
 
-const interval = setInterval(() => {
+let calls = 0;
+
+const d3Timer = timer((elapsed) => {
 	if (last - 1 < rects.length) {
-		rects[last - 1].render();
-	}
+		rects[last - 1].update({ fill: getFill(count)});
+		calls++;
+	} 
 	last = lfsr(last, tap);
 	if (last === first) {
 		count++;
+		tap = parseInt(taps[count], 16);
+		console.log('avg: ', elapsed / calls);
 	}
 	if (count > 5) {
-		clearInterval(interval);
+		d3Timer.stop();
 	}
-}, 30);
+});
